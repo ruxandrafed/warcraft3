652be3dc82a639b854839882cf5415bf7c430ea7
Ruxandra F. <ruxandra.fediuc@gmail.com>
2015-09-15 11:18:09 -0700

    Minor refactoring

--- lib/peasant.rb
+++ lib/peasant.rb
@@ -3,14 +3,11 @@ require_relative 'unit'
 
 class Peasant < Unit
 
-  attr_reader :health_points
+  attr_reader :health_points, :attack_power
 
   def initialize
     @health_points = 35
-  end
-
-  def attack_power
-    0
+    @attack_power = 0
   end
 
 end
